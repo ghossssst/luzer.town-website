74a20aecdb20025307266e04419e6dbb841c202a
Luke Wheeler <115876478+ghossssst@users.noreply.github.com>
2024-05-31 20:33:43 +0100

    added projects page + IP

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function hideLargeImage() {
   var overlay = document.getElementById('overlay');
   overlay.style.display = 'none';
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const ipAddressElement = document.getElementById('ip-address');
+
+  fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+          ipAddressElement.textContent = data.ip;
+      })
+      .catch(error => {
+          ipAddressElement.textContent = 'Unable to fetch IP address';
+          console.error('Error fetching IP address:', error);
+      });
+});
+
+let el = document.getElementById('visual5');
+el.style.setProperty('--rand', Math.random());
