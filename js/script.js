1bb50e33e8ecaa0557f61ca7e7ce5796618b7ef8
Luke Wheeler <115876478+ghossssst@users.noreply.github.com>
2024-06-01 18:51:43 +0100

    file system organised, links updated, styles added.

---
+++ js/script.js
@@ -0,0 +1,79 @@
+function onPageLoaded() {
+  console.log("page loaded");
+  displayBrowserInfo();
+  loadImagesWithProgress();
+}
+
+function showLargeImage(imageSrc) {
+  var largeImage = document.getElementById('largeImage');
+  largeImage.src = imageSrc;
+
+  var overlay = document.getElementById('overlay');
+  overlay.style.display = 'block';
+}
+
+function hideLargeImage() {
+  var overlay = document.getElementById('overlay');
+  overlay.style.display = 'none';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const ipAddressElement = document.getElementById('ip-address');
+
+  fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+          ipAddressElement.textContent = data.ip;
+      })
+      .catch(error => {
+          ipAddressElement.textContent = 'Unable to fetch IP address';
+          console.error('Error fetching IP address:', error);
+      });
+});
+
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  alert('Email address copied to clipboard: ' + text);
+}
+
+function loadImagesWithProgress() {
+  const images = document.querySelectorAll('.image-grid img');
+  const progressBar = document.getElementById('progress-bar');
+  const progressBarContainer = progressBar.parentElement;
+  let imagesLoaded = 0;
+
+  function updateProgressBar() {
+      const progress = (imagesLoaded / images.length) * 100;
+      progressBar.style.width = progress + '%';
+      progressBar.textContent = Math.round(progress) + '%';
+
+      if (imagesLoaded === images.length) {
+          setTimeout(() => {
+              progressBarContainer.style.display = 'none';
+          }, 500); 
+      }
+  }
+
+  images.forEach(image => {
+      if (image.complete) {
+          imagesLoaded++;
+          updateProgressBar();
+      } else {
+          image.onload = () => {
+              imagesLoaded++;
+              updateProgressBar();
+          };
+          image.onerror = () => {
+              imagesLoaded++;
+              updateProgressBar();
+          };
+      }
+  });
+
+  updateProgressBar();
+}
